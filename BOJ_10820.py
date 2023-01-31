1e425e7f30d766cb4db2f45d86796934f63ad541
YoonJungHo <wjdgh8926@naver.com>
2023-02-01 01:04:29 +0900

    10820

---
+++ BOJ_10820.py
@@ -0,0 +1,26 @@
+import sys
+
+while True:
+
+    lower_cnt, upper_cnt, num_cnt, space_cnt = 0, 0, 0, 0
+
+    S = sys.stdin.readline().strip()
+    print(S)
+
+    if not S:
+        break
+
+    for c in S:
+        if c.isalpha():
+            if ord(c) >= 97:
+                lower_cnt += 1
+            else:
+                upper_cnt += 1
+        elif c == ' ':
+            space_cnt += 1
+        else:
+            num_cnt +=1
+
+    print(lower_cnt, upper_cnt, num_cnt, space_cnt)
+
+    
